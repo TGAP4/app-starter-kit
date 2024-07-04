1120a5c13079086bdb495b1dd0e90f4fa6d26b29
Andrew Park <tgap9001@gmail.com>
2024-07-04 02:28:40 -0400

    idk

--- src/server/schema/builder.ts
+++ src/server/schema/builder.ts
@@ -1,30 +1,10 @@
 import SchemaBuilder from '@pothos/core';
 import PrismaPlugin from '@pothos/plugin-prisma';
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import {GraphQLContext} from '../context';
-import {PrismaClient} from '@prisma/client/extension';
 
-export let prismaa: PrismaClient;
-
-async function initPrisma() {
-  const { PrismaClient } = await import('@prisma/client');
-  prismaa = new PrismaClient();
-}
-
-initPrisma().catch((e) => {
-  console.error('Failed to initialize Prisma Client', e);
-});
-
-async function prisma() {
-  await initPrisma();
-  console.log(prismaa)
-  return prismaa;
-}
-
-export { prisma };
-
-// export const prisma = new PrismaClient({});
+export const prisma = new PrismaClient({});
 
 const builder = new SchemaBuilder<{
   Context: GraphQLContext
@@ -32,7 +12,7 @@ const builder = new SchemaBuilder<{
 }>({
   plugins: [PrismaPlugin],
   prisma: {
-    client: prismaa,
+    client: prisma,
     // Use where clause from prismaRelatedConnection for totalCount (will true by default in next major version)
     filterConnectionTotalCount: true,
     // Warn when not using a query parameter correctly
